612dcc5ee85a78a7ab44b7e689daca1891103fc0
Saku Hakamäki <saku.hakamaki66@gmail.com>
2022-05-26 11:51:18 +0300

    Adds step 1 of Anecdotes-task.

---
+++ part1/anekdootit/src/App.js
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+
+// Palauttaa satunnaisluvun minimin 0 ja maksimin 6 välillä, 
+// jotka vastaavat anecdotes-taulukossa olevien tekstien indeksejä.
+const getRandomNumber = () => {
+  return (
+    Math.floor(Math.random() * 7)
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
+  // Napin tapahtumankäsittelijä.
+  const handleClick = () => {
+    
+    // Haetaan satunnaisluku ja tallennetaan se muuttujaan. 
+    let randomNumber = getRandomNumber()
+    
+    // Haetaan sopivaa satunnaislukua kunnes sellainen on löytynyt.
+    while (true) {
+
+      // Jos arvottu satunnaisluku on sama kuin jo ruudulla näkyvä luku,
+      // haetaan uusi satunnaisluku ja tulostetaan siitä ilmoitus konsoliin.
+      if (randomNumber === selected) {
+        randomNumber = getRandomNumber()
+        console.log('Regenerated number.') 
+
+      // Muussa tapauksessa tulostetaan konsoliin käytetty satunnaisluku 
+      // ja asetetaan ruudulle uusi anekdootti setSelected-funktion avulla.
+      // Päädytty haluttuun lopputulokseen; poistutaan silmukasta.
+      } else {
+        console.log('Random number:', randomNumber)
+        setSelected(randomNumber)
+        break
+      }
+    }
+  }
+
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleClick}>Next anecdote</button>
+    </div>
+  )
+}
+
+export default App
