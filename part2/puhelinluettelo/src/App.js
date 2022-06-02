fb69c1c2e4165d30565eb6dfe691ed6cb3fc6ee0
Saku Hakamäki <saku.hakamaki66@gmail.com>
2022-06-02 12:02:42 +0300

    Phonebook-task: adds step 6.

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,19 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  // Haetaan Effect-hookilla yhteystiedot paikalliselta
+  // palvelimelta ja asetetaan ne tilaan ruudulle.
+  const hook = () => {
+    console.log('Effect.')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('Promise fulfilled.')
+        setPersons(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
   // Lomakkeen tapahtumankäsittelijä.
   const addName = (event) => {
