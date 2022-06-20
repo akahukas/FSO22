c2718bbf7462fcdd3b942cf4ad0ccda0e326b3e1
Saku Hakamäki <saku.hakamaki66@gmail.com>
2022-06-20 17:23:08 +0300

    Step 7 of Bloglist frontend -task.

--- part5/blogilista-frontend/src/components/Blog.js
+++ part5/blogilista-frontend/src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [minimized, setMinimized] = useState(true)
+
+  const hideWhenMinimized = { display: minimized ? 'none' : ''}
+
+  const toggleSize = () => {
+    setMinimized(!minimized)
+  }
+
+  const blogStyle = {
+    border: 'solid',
+    borderWidth: 1,
+    padding: 10,
+    margin: 5,
+  }
+
+  return (
+  <div style={blogStyle}>
+    <div>
+      {blog.title} {blog.author} <button onClick={toggleSize}>{minimized ? 'view' : 'hide'}</button>
+    </div>
+    <div style={hideWhenMinimized}>
+      <p>{blog.url}</p>
+      likes {blog.likes} <button>like</button>
+      <p>{blog.user.name}</p>
+    </div>
+    
   </div>  
-)
+)}
 
 export default Blog
