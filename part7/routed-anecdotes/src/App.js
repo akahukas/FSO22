57f22e6891727c78b6b99da9e1cfb75446cdd6d7
Saku Hakamäki <saku.hakamaki66@gmail.com>
2022-07-06 12:41:52 +0300

    Anecdotes-app and Hooks -task: adds step 2. (7.5)

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -98,7 +98,14 @@ const CreateNew = (props) => {
           url for more info
           <input {...info} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type='reset' onClick={() => {
+          content.reset()
+          author.reset()
+          info.reset()
+        }}>
+          reset
+        </button>
       </form>
     </div>
   )
