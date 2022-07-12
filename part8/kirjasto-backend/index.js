dd734dd126f61cc8c3d62afab053f4abb0d27c4a
Saku Hakamäki <saku.hakamaki66@gmail.com>
2022-07-12 16:36:11 +0300

    Adds step 1 of Database -task. (8.13)

--- part8/kirjasto-backend/index.js
+++ part8/kirjasto-backend/index.js
@@ -1,93 +1,31 @@
-const { ApolloServer, gql } = require('apollo-server')
-const { v1: uuid } = require('uuid')
+const {
+  ApolloServer,
+  UserInputError,
+  AuthenticationError,
+  gql
+} = require('apollo-server')
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
-*/
+const Author = require('./models/author')
+const Book = require('./models/book')
 
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
+const MONGODB_URI = process.env.MONGODB_URI
+
+// Yhdistetään MongoDB-tietokantaan.
+console.log('Connecting to:', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB.')
+  })
+  .catch((error) => {
+    console.log(
+      'An error occurred while trying to connect to MongoDB:',
+      error.message
+    )
+  })
 
 // Skeemat eli sovelluksen datan muotoilun 
 // määrittely clientin ja palvelimen välillä.
@@ -102,7 +40,7 @@ const typeDefs = gql`
   type Book {
     title: String!
     published: Int!
-    author: String!
+    author: Author!
     id: ID!
     genres: [String!]!
   }
@@ -132,14 +70,14 @@ const typeDefs = gql`
 // Resolverit eli miten kyselyihin vastataan.
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
-    allBooks: (root, args) => {
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
       
       // Jos kirjailijaa ei annettu
       // parametrina, palautetaan kaikki kirjat.
       if (!args.author && !args.genre) {
-        return books
+        return Book.find({})
       }
       // Jos parametrina annettu vain kirjailija,
       // palautetaan kyseisen kirjailijan kirjoittamat kirjat.
@@ -169,7 +107,7 @@ const resolvers = {
         )
       }
     },
-    allAuthors: () => authors,
+    allAuthors: async () => Author.find({}),
   },
   Author: {
     bookCount: (root) => (
@@ -177,35 +115,51 @@ const resolvers = {
     )
   },
   Mutation: {
-    addBook: (root, args) => {
+    addBook: async (root, args) => {
 
-      // Tallennetaan taulukkoon kaikkien
-      // tiedossa olevien kirjailijoiden nimet.
-      const authorNames = authors.map((author) => author.name)
+      // Tarkistetaan tietokannasta, onko parametrina
+      // saatua nimeä vastaava kirjailija jo olemassa.
+      let authorInDatabase = await Author.findOne({ name: args.author })
 
       // Jos parametrina saatu kirjailija ei ole
       // tiedossa, lisätään hänet järjestelmään.
-      if (!authorNames.includes(args.author)) {
+      if (!authorInDatabase) {
         
-        // Luodaan kirjailijaolio; nimi annettu parametreissa,
-        // syntymävuodesta ei tietoa. Lisäksi määritetään uniikki id.
-        const author = {
+        // Luodaan kirjailijaolio; nimi annettu
+        // parametreissa, syntymävuodesta ei tietoa.
+        const author = new Author({
           name: args.author,
-          id: uuid(),
           born: null
+        })
+        
+        // Yritetään kirjailijan tallentamista tietokantaan.
+        try {
+          authorInDatabase = await author.save()
         }
-        // Lisätään luotu olio kirjailijoiden perälle.
-        authors = authors.concat(author)
+        // Virheen sattuessa reagoidaan virheellä.
+        catch (error) {
+          throw new UserInputError(error.message, {
+            invalidArgs: args,
+          })
+        }
       }
 
-      // Luodaan uusi kirjaolio parametreina saaduista
-      // arvoista, sekä annetaan lisäksi uniikki id.
-      const book = { ...args, id: uuid() }
+      // Luodaan uusi kirjaolio parametreina saaduista arvoista,
+      // sekä liitetään juuri luodun kirjailijaolion tunniste kirjaolioon.
+      const book = new Book({
+        ...args, author: authorInDatabase._id
+      })
 
-      // Lisätään luotu olio kirjojen perälle,
-      // ja palautetaan luotu kirja.
-      books = books.concat(book)
-      return book
+      // Yritetään kirjan tallentamista tietokantaan.
+      try {
+        return book.save()
+      }
+      // Virheen sattuessa heitetään virhe.
+      catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
     },
     editAuthor: (root, args) => {
 
