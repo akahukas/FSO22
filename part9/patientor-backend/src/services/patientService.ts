64dccfd15249b92f164ffc49df112493eb63d561
Saku Hakamäki <saku.hakamaki66@gmail.com>
2022-07-19 14:05:09 +0300

    Patientor backend -task: adds step 4. (9.11)

---
+++ part9/patientor-backend/src/services/patientService.ts
@@ -0,0 +1,27 @@
+import patients from "../../data/patients";
+import { Patient } from "../types";
+import { NonSensitivePatient } from "../types";
+
+const getEntries = (): Array<Patient> => {
+  return patients;
+};
+
+const getNonSensitiveEntries = (): NonSensitivePatient[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+const addPatient = () => {
+  return null;
+};
+
+export default {
+  getEntries,
+  getNonSensitiveEntries,
+  addPatient
+};
